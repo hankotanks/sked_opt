8e64fe81a0a86f1e706c75d7efd1d4803cab88f1
Hank Lewis <hank@modernatomic.com>
2025-08-25 22:40:15 +0200

    partial implementation of basic ILP

--- include/ilp.h
+++ include/ilp.h
@@ -5,32 +5,65 @@
 
 #include "hh.h"
 
+#include "station.h"
+#include "source.h"
+
 typedef struct {
     size_t count_b, count_k, count_t;
     size_t count_x, count_y, count_z;
     lprec* rec;
     double* buf;
-    size_t* map_sta;
-    size_t* map_src;
+    size_t* map_sta_to_idx;
+    size_t* map_src_to_idx;
+    size_t* map_idx_to_sta;
+    size_t* map_idx_to_src;
 } Program;
 
+// Program
 void
 Program_init(Program* ilp);
+bool // returns truthy if the solution is optimal
+Program_solve(const Program* const ilp);
 void
 Program_free(Program* const ilp);
 void
 Program_dump(const Program* const ilp);
 
+// Program helper functions
+Station
+Program_idx_to_sta(const Program* const ilp, size_t bi);
+Source
+Program_idx_to_src(const Program* const ilp, size_t ki);
+
+// constraints
 void
-constr_begin(Program* const ilp);
+row_begin(Program* const ilp);
 void
-constr_x(Program* const ilp, double coeff, size_t t, size_t k, size_t b);
+row_end_as_constr(Program* const ilp, int constr_type, double rhs);
 void
-constr_y(Program* const ilp, double coeff, size_t t, size_t k);
+row_end_as_obj(Program* const ilp);
+// group of functions for indexing using net and sky indices
 void
-constr_z(Program* const ilp, double coeff, size_t b, size_t c);
+row_x(Program* const ilp, double coeff, size_t t, size_t k, size_t b);
 void
-constr_end(Program* const ilp, int constr_type, double rhs);
+row_y(Program* const ilp, double coeff, size_t t, size_t k);
+void
+row_z(Program* const ilp, double coeff, size_t b, size_t c);
+// indexing using internal indices
+void
+row_xi(Program* const ilp, double coeff, size_t t, size_t ki, size_t bi);
+void
+row_yi(Program* const ilp, double coeff, size_t t, size_t ki);
+void
+row_zi(Program* const ilp, double coeff, size_t bi, size_t c);
+
+//
+// declarations for different ILP configurations
+//
+
+// basic ILP using only sky coverage as scoring function
+void
+Program_load_skycov(Program* const ilp); 
 
 
 #endif // ILP_H__
--- src/sched.c
+++ src/sched.c
@@ -1,5 +1,7 @@
 #include "sched.h"
 
+#include <stdarg.h>
+
 #include <lp_lib.h>
 
 #include "hh.h"
@@ -11,41 +13,11 @@ sched_start(void) {
     Program ilp;
     Program_init(&ilp);
     Program_dump(&ilp);
+    HH_DBG("Loading ILP");
+    Program_load_skycov(&ilp);
+    HH_DBG("Running ILP");
+    if(Program_solve(&ilp)) HH_DBG("Optimal solution found");
+    else HH_DBG("Unable to find optimal solution");
     Program_free(&ilp);
-
-#if 0
-    lprec* lp = make_lp(0, 0);
-    HH_ASSERT(lp != NULL, "Failed to construct ILP.");
-
-    set_col_name(lp, 1, "x");
-    set_col_name(lp, 2, "y");
-
-    double row[3], var[2];
-#if 0
-    max: 143 x + 60 y; 
-    120 x + 210 y <= 15000; 
-    110 x + 30 y <= 4000; 
-    x + y <= 75;
-#endif
-    row[1] = 143.0;
-    row[2] = 60.0;
-    set_obj_fn(lp, row);
-    set_maxim(lp);
-    row[1] = 120.0; row[2] = 210.0;
-    add_constraint(lp, row, LE, 15000);
-    row[1] = 110.0; row[2] = 30.0;
-    add_constraint(lp, row, LE, 4000);
-    row[1] = 1.0; row[2] = 1.0;
-    add_constraint(lp, row, LE, 75);
-    if (solve(lp) == OPTIMAL) {
-        get_variables(lp, var);
-        printf("Optimal value: %f\n", get_objective(lp));
-        printf("x = %f\n", var[0]);
-        printf("y = %f\n", var[1]);
-    } else {
-        printf("No optimal solution found.\n");
-    }
-
-    delete_lp(lp);
-#endif
 }
+
